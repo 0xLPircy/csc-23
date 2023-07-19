5796cdde86e95bd586e932bac0bb4f90b79748a2
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-19 19:44:30 +0530

    files +nav

---
+++ app/components/Nav.jsx
@@ -0,0 +1,102 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
+
+
+    return (
+        <nav className="flex justify-center items-center py-3  border-b-[1px] border-[#f9672d3c]">
+            <div className="">
+                <Link href="/">
+                    {/* <img src={ } alt="logo" /> */}
+                    <Image width={210} height={210} src="./nav_logo.svg" />
+                </Link>
+            </div>
+            <ul className="flex justify-center items-center">
+                <li className="p-[12px]" key={`home`}>
+                    <Link href="/">
+                        Home
+                        <div />
+                    </Link>
+                </li>
+                <li className="p-[12px]" key={`events`}>
+                    <Link href="/">
+                        Events
+                        <div />
+                    </Link>
+                </li>
+                <li className="p-[12px]" key={`membership`}>
+                    <Link href="/">
+                        Membership
+                        <div />
+                    </Link>
+                </li>
+                <li className="p-[12px]" key={`newsletter`}>
+                    <Link href="/">
+                        Newsletter
+                        <div />
+                    </Link>
+                </li>
+                <li className="p-[12px]" key={`events`}>
+                    <Link href="/">
+                        Team
+                        <div />
+                    </Link>
+                </li>
+                <li key={`insta`}>
+                    <a
+                        href="https://www.instagram.com/csc_muj/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                    </a>
+                </li>
+            </ul>
+
+            <div className="">
+
+                {/* {toggle && (
+                    <div>
+                        <ul>
+                            <li key={`mobile-home`}>
+                                <Link href="/" onClick={() => setToggle(false)}>
+                                    home
+                                </Link>
+                            </li>
+                            <li key={`mobile-events`}>
+                                <Link href="/" onClick={() => setToggle(false)}>
+                                    Events
+                                </Link>
+                            </li>
+                            <li key={`mobile-membership`}>
+                                < Link href="/" onClick={() => setToggle(false)}>
+                                    membership
+                                </Link>
+                            </li>
+                            <li key={`mobile-newsletter`}>
+                                <Link href="/" onClick={() => setToggle(false)}>
+                                    newsletter
+                                </Link>
+                            </li>
+                            <li key={`mobile-team`}>
+                                <Link href="/" onClick={() => setToggle(false)}>
+                                    team
+                                </Link>
+                            </li>
+                            <li key={`mobile-insta`}>
+                                <Link href="/">
+                                    insta
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                )} */}
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Nav from './components/Nav'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Nav />
+        {children}
+      </body>
     </html>
   )
 }
