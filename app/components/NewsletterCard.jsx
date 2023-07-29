56abeed56cdf9baa37e0943426d53511f598bfca
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-29 18:56:18 +0530

    newsletter card styling

--- app/components/NewsletterCard.jsx
+++ app/components/NewsletterCard.jsx
@@ -4,13 +4,15 @@ import cs from "../../assets/cscmuj.gif";
 const NewsletterCard = ({ title, date, description, nlink }) => {
   return (
     <>
-      <div className="card">
+      <div className="card bg-[#e3e3e3] p-3 m-6 text-[#121212] rounded-lg grid grid-flow-row justify-center items-center">
         <img src={cs} alt="" />
-        <h3>{title}</h3>
+        <h3 className="text-3xl justify-self-center">{title}</h3>
         {/* use slice when states */}
-        <p>{date}</p>
-        <p>{description}</p>
-        <a href={nlink}>Read more</a>
+        <p className="text-xs justify-self-start">{date}</p>
+        <p className="justify-self-center text-sm">{description}</p>
+        <a href={nlink} className="justify-self-end text-xs italic">
+          Read more...
+        </a>
       </div>
     </>
   );
