97a2964516d0de459df7ce308266f2cdb651e3db
L-Pircy <taveesha.agarwal@gmail.com>
2023-08-01 09:58:45 +0530

    team struct

--- app/team/page.jsx
+++ app/team/page.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import h1 from "@/assets/team/behindTheScene.svg";
 import h2 from "@/assets/team/facultyCoordinator.svg";
-import p1 from "@/assets/";
+import h3 from "@/assets/team/dsw.svg";
+import h4 from "@/assets/team/executive.svg";
+import h5 from "@/assets/team/core.svg";
 
 import Image from "next/image";
 
 const TeamPage = () => {
   return (
-    <div className="grid grid-flow-row justify-center items-center pt-[90px]">
+    <div className="grid grid-flow-row justify-center items-center pt-[90px] text-[#eeeeee]">
       <div className="justify-self-center">
         <Image height={600} width={600} src={h1} />
       </div>
@@ -20,6 +22,157 @@ const TeamPage = () => {
           <div>
             <Image height={60} width={60} />
             <Image height={60} width={60} />
+          </div>
+        </div>
+        <div>
+          <Image height={120} width={120} />
+          <h3>Dr. Roheet Bhatnagar</h3>
+          <h4>Director of Research</h4>
+          <div>
+            <Image height={60} width={60} />
+            <Image height={60} width={60} />
+          </div>
+        </div>
+      </div>
+      <div className="pageSeperator" />
+      <div>
+        <div>
+          <Image height={120} width={120} />
+          <h3>Dr. Roheet Bhatnagar</h3>
+          <h4>Director of Research</h4>
+          <div>
+            <Image height={60} width={60} />
+            <Image height={60} width={60} />
+          </div>
+        </div>
+        <Image height={600} width={300} src={h3} />
+      </div>
+      <div className="pageSeperator" />
+      <div>
+        <Image height={300} width={600} src={h4} />
+        <div>
+          <div>
+            <Image height={120} width={120} />
+            <h3>Dr. Roheet Bhatnagar</h3>
+            <h4>Director of Research</h4>
+            <div>
+              <Image height={60} width={60} />
+              <Image height={60} width={60} />
+            </div>
+          </div>
+          <div>
+            <Image height={120} width={120} />
+            <h3>Dr. Roheet Bhatnagar</h3>
+            <h4>Director of Research</h4>
+            <div>
+              <Image height={60} width={60} />
+              <Image height={60} width={60} />
+            </div>
+          </div>
+          <div>
+            <Image height={120} width={120} />
+            <h3>Dr. Roheet Bhatnagar</h3>
+            <h4>Director of Research</h4>
+            <div>
+              <Image height={60} width={60} />
+              <Image height={60} width={60} />
+            </div>
+          </div>
+          <div>
+            <Image height={120} width={120} />
+            <h3>Dr. Roheet Bhatnagar</h3>
+            <h4>Director of Research</h4>
+            <div>
+              <Image height={60} width={60} />
+              <Image height={60} width={60} />
+            </div>
+          </div>
+          <div>
+            <Image height={120} width={120} />
+            <h3>Dr. Roheet Bhatnagar</h3>
+            <h4>Director of Research</h4>
+            <div>
+              <Image height={60} width={60} />
+              <Image height={60} width={60} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pageSeperator" />
+      <div>
+        <Image height={300} width={600} src={h5} />
+        <div>
+          <div>
+            <Image height={120} width={120} />
+            <h3>Dr. Roheet Bhatnagar</h3>
+            <h4>Director of Research</h4>
+            <div>
+              <Image height={60} width={60} />
+              <Image height={60} width={60} />
+            </div>
+          </div>
+          <div>
+            <Image height={120} width={120} />
+            <h3>Dr. Roheet Bhatnagar</h3>
+            <h4>Director of Research</h4>
+            <div>
+              <Image height={60} width={60} />
+              <Image height={60} width={60} />
+            </div>
+          </div>
+          <div>
+            <Image height={120} width={120} />
+            <h3>Dr. Roheet Bhatnagar</h3>
+            <h4>Director of Research</h4>
+            <div>
+              <Image height={60} width={60} />
+              <Image height={60} width={60} />
+            </div>
+          </div>
+          <div>
+            <Image height={120} width={120} />
+            <h3>Dr. Roheet Bhatnagar</h3>
+            <h4>Director of Research</h4>
+            <div>
+              <Image height={60} width={60} />
+              <Image height={60} width={60} />
+            </div>
+          </div>
+          <div>
+            <Image height={120} width={120} />
+            <h3>Dr. Roheet Bhatnagar</h3>
+            <h4>Director of Research</h4>
+            <div>
+              <Image height={60} width={60} />
+              <Image height={60} width={60} />
+            </div>
+          </div>
+          <div>
+            <Image height={120} width={120} />
+            <h3>Dr. Roheet Bhatnagar</h3>
+            <h4>Director of Research</h4>
+            <div>
+              <Image height={60} width={60} />
+              <Image height={60} width={60} />
+            </div>
+          </div>
+          <div>
+            <Image height={120} width={120} />
+            <h3>Dr. Roheet Bhatnagar</h3>
+            <h4>Director of Research</h4>
+            <div>
+              <Image height={60} width={60} />
+              <Image height={60} width={60} />
+            </div>
+          </div>
+          <div>
+            <Image height={120} width={120} />
+            <h3>Dr. Roheet Bhatnagar</h3>
+            <h4>Director of Research</h4>
+            <div>
+              <Image height={60} width={60} />
+              <Image height={60} width={60} />
+            </div>
           </div>
         </div>
       </div>
