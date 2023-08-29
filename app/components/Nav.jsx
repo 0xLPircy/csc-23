c2445fffa2f056c5330d4a5434183461ce638a46
L-Pircy <taveesha.agarwal@gmail.com>
2023-08-29 12:27:06 +0530

    newsletter in landing

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <nav className="grid grid-flow-col grid-cols-4 fixed justify-center px-6 py-3 border-b-[1px] border-[#f9672d3c] text-white w-[100vw] nav">
-      <div className="c col-span-1">
+      <div className="col-span-1">
         <Link href="/">
           {/* <img src={ } alt="logo" /> */}
           <Image width={210} height={210} src="./nav_logo.svg" />
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,30 @@
+"use client";
+import NewsletterCard from "./components/NewsletterCard";
 import AboutUs from "./containers/AboutUs";
 import CyberSec from "./containers/CyberSec";
 import Header from "./containers/Header";
 import InstaPosts from "./containers/InstaPosts";
 import LatestCoverage from "./containers/LatestCoverage";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [newsletters, setNewsletters] = useState([]);
+  useEffect(() => {
+    // Fetch the JSON data when the component mounts
+    fetchNewsletters();
+  }, []);
+
+  const fetchNewsletters = async () => {
+    try {
+      const response = await fetch("./newsletters.json");
+      // console.log(response);
+      const data = await response.json();
+      // console.log(data);
+      setNewsletters(data);
+    } catch (error) {
+      console.error("Error fetching JSON:", error);
+    }
+  };
   return (
     <>
       <div className="pt-12">
@@ -15,6 +35,22 @@ export default function Home() {
         <CyberSec />
         <div className="pageSeparator"></div>
         <InstaPosts />
+        <div className="pageSeparator"></div>
+        <div className="landingNewsletter grid grid-flow-col grid-cols-3">
+          {newsletters.map((newsletter) => (
+            <div
+              key={newsletter.title}
+              className="newsletter-card text-[#f3f3f3]"
+            >
+              <NewsletterCard
+                title={newsletter.title}
+                date={newsletter.date}
+                description={newsletter.description}
+                nlink={newsletter.link}
+              />
+            </div>
+          ))}
+        </div>
         <div className="pageSeparator"></div>
         <LatestCoverage />
         <div className="pageSeparator"></div>
